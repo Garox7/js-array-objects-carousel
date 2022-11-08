80b4dd0292933076e3363ef47174ee841a25f157
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-11-08 23:19:04 +0100

    Js Add

--- javascript/script.js
+++ javascript/script.js
@@ -25,3 +25,82 @@ const myArray = [
 		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
 	},
 ];
+
+const heroPreview = document.querySelector('.hero__preview');
+const heroThumb = document.querySelector('.hero__thumb');
+const btnUp = document.querySelector('.control-up');
+const btnDown = document.querySelector('.control-down');
+
+function createHerotext(object) {
+    
+}
+
+myArray.forEach ((object, i) => {
+    
+    const imgHero = document.createElement('img');
+    imgHero.src = `img/${object.image}`;
+    
+    const heroText = document.createElement('div');
+    heroText.classList.add('hero__preview__text');
+    heroText.innerHTML = `<h1>${object.title}</h1><p>${object.text}</p>`;
+    
+    const imgThumb = document.createElement('img');
+    imgThumb.src = `img/${object.image}`;
+    
+    if (i === 0) {
+        imgHero.classList.add('active');
+        imgThumb.classList.add('selected');
+        heroText.classList.add('show');
+
+    }
+    
+    heroPreview.append(heroText);
+    heroPreview.append(imgHero);
+    heroThumb.append(imgThumb);
+});
+
+const listHeroImages = heroPreview.querySelectorAll('img');
+const listThumbImages = heroThumb.querySelectorAll('img');
+const listTextImages = heroPreview.querySelectorAll('.hero__preview__text')
+
+let indexImages = 0;
+
+btnUp.addEventListener('click', function () {
+
+    listHeroImages[indexImages].classList.remove('active');
+    listThumbImages[indexImages].classList.remove('selected');
+    listTextImages[indexImages].classList.remove('show');
+
+
+    if (indexImages === 0) {
+        indexImages = listHeroImages.length;
+    }
+    indexImages--;
+
+    listHeroImages[indexImages].classList.add('active');
+    listThumbImages[indexImages].classList.add('selected');
+    listTextImages[indexImages].classList.add('show');
+
+
+    console.log(indexImages);
+});
+
+btnDown.addEventListener('click', function() {
+
+    listHeroImages[indexImages].classList.remove('active');
+    listThumbImages[indexImages].classList.remove('selected');
+    listTextImages[indexImages].classList.remove('show');
+
+
+    indexImages++;
+    if (indexImages === listHeroImages.length) {
+        indexImages = 0;
+    }
+
+    listHeroImages[indexImages].classList.add('active');
+    listThumbImages[indexImages].classList.add('selected');
+    listTextImages[indexImages].classList.add('show');
+
+
+    console.log(indexImages);
+})
